b4f6e2beba8b810c68bb6d0ad729e81ab93a5881
vitor estevam <vitorestevam02@gmail.com>
2021-11-14 02:30:24 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+)
+
+type Message struct {
+	Content string
+	Moment  string
+}
+
+var Messages []Message
+
+func saveMessages() {
+	m, _ := json.Marshal(Messages)
+	os.WriteFile("data.txt", m, 0644)
+}
+
+func loadMessages() {
+	data, _ := os.ReadFile("data.txt")
+	json.Unmarshal(data, &Messages)
+}
+
+func main() {
+
+	loadMessages()
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			responseMessage := r.FormValue("message")
+			if responseMessage != "" {
+				moment := time.Now()
+				message := Message{responseMessage, moment.Format("2006-01-02 15:04:05")}
+				Messages = append([]Message{message}, Messages...)
+				saveMessages()
+			}
+		}
+
+		pageData := struct{ Messages []Message }{Messages}
+		tmpl.Execute(w, pageData)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Go guestbook
+
+[PHP Version](https://github.com/hackergaucho/guest) by "Hacker gaucho"
+
+[Ruby Version](https://github.com/leandronsp/guestbook) by "leandronsp"
+
+![picture 1](images/screenshot.png)  
+
+## refs
+
+- https://gowebexamples.com/templates/
+- https://gowebexamples.com/forms/
+- https://gobyexample.com/reading-files
+- https://gobyexample.com/writing-files
+- https://stackoverflow.com/questions/53737435/how-to-prepend-int-to-slice
